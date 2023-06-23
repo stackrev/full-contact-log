90dd0651e00b2c9a919578bf5b45334877c69c6b
stackrev <farbitherast5888@gmail.com>
2023-06-23 11:50:31 +0000

    log visitors

--- index.js
+++ index.js
@@ -6,8 +6,23 @@ const app = express();
 // Middleware to parse json
 app.use(bodyParser.json());
 
+let recent_history = [];
+
 app.post('/api/data', (req, res) => {
   const { pid, date } = req.body;
+  if(!pid) return;
+  const jsonObject = JSON.parse(pid);
+  const personId = jsonObject.personIds[0];
+
+  const date1 = new Date(recent_history[personId]);
+  const date2 = new Date(date1);
+
+  if(recent_history[personId] && date2.getTime() - date1.getTime() < 3600 * 1000){
+    recent_history[personId] = date;
+    res.json({ status: 'success', message: 'Data duplicated so ignored' });
+    return;
+  }
+  recent_history[personId] = date;
 
   // Verify that both pid and date were provided
   if (!pid || !date) {
@@ -16,7 +31,7 @@ app.post('/api/data', (req, res) => {
   }
 
   // Do something with the pid and date here
-  console.log(pid, date);
+  console.log(personId, date);
 
   // Send a response back to the client
   res.json({ status: 'success', message: 'Data received successfully' });
